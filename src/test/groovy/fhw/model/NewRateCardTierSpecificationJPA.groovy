1f1d1d3e6390b0c8b9e1e22ae1bcfbcb24807aac
fwelland <fwelland@promnetwork.com>
2016-06-08 17:36:48 -0400

    re-working the cascading stuff to search for workable solution.

--- src/test/groovy/fhw/model/NewRateCardTierSpecificationJPA.groovy
+++ src/test/groovy/fhw/model/NewRateCardTierSpecificationJPA.groovy
@@ -53,7 +53,6 @@ class NewRateCardTierSpecificationJPA
         sql?.close()
     }
             
-    @IgnoreRest    
     def "NIP: do a remove then persist of a rate card w tiers where there was a tier delete then add using em remove and persist and no cascade"()
     {
         when:     "pretend like we ask services for RateCard object with tiers"
@@ -130,45 +129,46 @@ class NewRateCardTierSpecificationJPA
                 2000000 == row.BALANCE_THRESHHOLD
             }        
     }
-
-    def "do a remove then merge of a rate card w tiers where there was a tier delete then add; this will clear tiers prior to merge"()
+    
+    def Exception "get causing exception"(Exception ex)
     {
-        def theBankId = -678
-        def progId =  57
-        def startId = -343
-        def rc = new RateCard()
-        rc.bankId = theBankId
-        rc.programId = 57
-        rc.name = "Hector"
-
-        4.times{
-            def tier = new ICSRateCardTier();
-            tier.id = startId
-            tier.bankId = theBankId
-            tier.programId = progId
-            tier.balanceThreshhold =  (1000000 * (it + 1))
-            tier.rateType = 1
-            tier.spread = 1
-            tier.fixedRate = 1
-            tier.createDate = LocalDateTime.now()
-            tier.updateDate = LocalDateTime.now()
-            tier.modUserId = "frankie ${it}"
-            startId += -1
-            rc.addTier(tier)
+        def e  = ex.getCause()
+        if(e)
+        {
+            e = "get causing exception"(e)
         }
-        em.getTransaction().begin()
-        em.persist(rc)
-        em.detach(rc)
-        em.getTransaction().commit()
+        else
+        {
+            e = ex
+        }
+        return e
+    }
 
-        when:
+    @IgnoreRest
+    def "NIP: load ratecard with cascade-all; remove a tier in memory; add one in; and then try to save/merger"()
+    {
+        when:     "pretend like we ask services for RateCard object with tiers"
+            em.getTransaction().begin()
+            def q = em.createNamedQuery("findByBankIdAndProgramId")
+            q.setParameter("bankId", theBankId)
+            q.setParameter("programId", progId)
+            def RateCard rc = q.getSingleResult()
+            em.getTransaction().commit()
+            
+        then:   "Lets inspect to make sure the rate card is loaded like we think..."
+            rc
+            "Hector" == rc.name
+            null != rc.tiers
+            rc.tiers.size() == 4 
+
+        when:   "Pretend like we are UI and we need to remove a tier" 
             def removedSomething = rc.tiers.removeAll{ it.balanceThreshhold == 2000000 }
 
-        then:
+        then:   "Lets check to see if the in-memory copy seems to have removed a tiers"
             removedSomething
             rc.tiers.size() == 3
 
-        when:
+        when:       "Pretend like UI is adding a new tier exactly like one removed (and tinker with ratecard)."        
             startId += -1
             rc.name = "Fido"
             def newTier = new ICSRateCardTier();
@@ -183,36 +183,20 @@ class NewRateCardTierSpecificationJPA
             newTier.updateDate = LocalDateTime.now()
             newTier.modUserId = "I am the new one"
             rc.addTier(newTier)
-            em.getTransaction().begin()
-            def q = em.createNamedQuery("removeTiers");
-            q.setParameter("bankId", theBankId)
-            q.setParameter("programId", progId)
-            def updateCount = q.executeUpdate()
-            em.flush()
-        then:
-            4 == updateCount
-            sql.rows("select * from ics_rt_crd_tier where bnk_Id = ?.bid and prgm_id = ?.pid", bid:rc.bankId, pid:rc.programId).size() == 0
 
-        when:
+        then:   "Lets check to see if that add worked"
+            rc.tiers.size() == 4
+            
+        when:    "Ok now lets pretend the UI is saving using only a mege"                    
+            em.getTransaction().begin()            
             em.merge(rc)
             em.getTransaction().commit()
-
-        then:
-            sql.rows("select * from ratecard where bankId = ?.bid and programId = ?.pid", bid:rc.bankId, pid:rc.programId).size() == 1
-            sql.rows("select * from ics_rt_crd_tier where bnk_Id = ?.bid and prgm_id = ?.pid", bid:rc.bankId, pid:rc.programId).size() == 4
-            sql.eachRow("select * from ratecard where bankId = ${theBankId} and programId = ${progId}")
-            { row ->
-                "Fido" == row.NAME
-            }
-            sql.eachRow("select * from ics_rt_crd_tier where ICS_RT_CRD_TIER_ID = ${startId}")
-            { row ->
-                "I am the new one"  == row.MOD_USR_ID
-                2000000 == row.BALANCE_THRESHHOLD
-            }
-
-        cleanup:
-            sql.execute("delete from ratecard where bankId = ?.bid and programId = ?.pid", bid:rc.bankId, pid:rc.programId)
-            sql.execute("delete from ics_rt_crd_tier where bnk_Id = ?.bid and prgm_id = ?.pid", bid:rc.bankId, pid:rc.programId)
+        
+        then:       "Let check to see we got a constraint violation"
+            RollbackException ex = thrown() 
+            def c = "get causing exception"(ex)
+            c
+            c instanceof SQLIntegrityConstraintViolationException            
     }
     
     def "use no cascade; do a remove then merge of a rate card w tiers where there was a tier delete then add; this will clear tiers prior to merge"()
